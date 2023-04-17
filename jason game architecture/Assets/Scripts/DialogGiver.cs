4c242d69acee0132a879f1b6d5e88e34474e7794
brennenfire <barbustefan2002@gmail.com>
2023-04-17 21:34:42 +0300

    simple dialog visibility solution

--- jason game architecture/Assets/Scripts/DialogGiver.cs
+++ jason game architecture/Assets/Scripts/DialogGiver.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class DialogGiver : MonoBehaviour
 {
     [SerializeField] TextAsset dialog;
+    [SerializeField] Canvas canvas;
 
     void OnTriggerEnter(Collider other)
     {
         var player = other.GetComponent<ThirdPersonMover>();
         if (player != null)
         {
+            canvas.gameObject.SetActive(true);
             FindObjectOfType<DialogController>().StartDialog(dialog);
             transform.LookAt(player.transform.position);
         }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        canvas.gameObject.SetActive(false);
     }
 }
