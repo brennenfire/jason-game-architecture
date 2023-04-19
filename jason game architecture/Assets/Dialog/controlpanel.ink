b1afbb769eb58b7804cd620a386c61de03c2d281
brennenfire <barbustefan2002@gmail.com>
2023-04-19 18:56:55 +0300

    implemented control panel's dialog

---
+++ jason game architecture/Assets/Dialog/controlpanel.ink
@@ -0,0 +1,22 @@
+ah of course, the panel that activates the acces to the mat
+no way to get the key to the door otherwise
+* activate panel 
+-> press
+
+== press ==
+* press the blue button 
+-> wrong
+* press the red button 
+-> correct
+* press the green button 
+-> wrong
+
+== wrong ==
+wrong button dumbass 
+-> press
+
+== correct ==
+great job, you pressed a button #E.ActivateMat
+-> END
+
+- -> END
--- jason game architecture/Assets/Scripts/KeyGiver.cs
+++ jason game architecture/Assets/Scripts/KeyGiver.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class KeyGiver : MonoBehaviour
 {
     [SerializeField] GameEvent doorEvent;
+    bool activated;
 
     void OnTriggerEnter(Collider other)
     {
         var player = other.GetComponent<ThirdPersonMover>();
-        if (player != null)
+        if (player != null && activated)
         {
             doorEvent.Invoke();
         }
+    }
+
+    public void Activate()
+    {
+        activated = true;
     }
 }
