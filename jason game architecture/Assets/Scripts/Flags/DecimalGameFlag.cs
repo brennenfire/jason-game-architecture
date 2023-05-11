f134eeed73c05fa95f72188b9c5f050b0db861e3
brennenfire <barbustefan2002@gmail.com>
2023-05-12 02:12:55 +0300

    added decimal and string flags

---
+++ jason game architecture/Assets/Scripts/Flags/DecimalGameFlag.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Decimal Game Flag")]
+public class DecimalGameFlag : GameFlag<decimal>
+{
+    public void Modify(decimal value)
+    {
+        Value += value;
+        SendChanged();
+    }
+}
---
+++ jason game architecture/Assets/Scripts/Flags/StringGameFlag.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Counted Int Game Flag")]
+public class StringGameFlag : GameFlag<string>
+{
+    public void Modify(string value)
+    {
+        Value = value;
+        SendChanged();
+    }
+}
