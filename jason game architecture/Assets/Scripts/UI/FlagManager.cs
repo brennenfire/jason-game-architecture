641bea721b79827c67912b606fb0e1ee94f01d99
brennenfire <barbustefan2002@gmail.com>
2023-05-15 21:13:57 +0300

    improved performance of my flag manager

--- jason game architecture/Assets/Scripts/UI/FlagManager.cs
+++ jason game architecture/Assets/Scripts/UI/FlagManager.cs
@@ -6,6 +6,8 @@ public class FlagManager : MonoBehaviour
 {
     [SerializeField] List<GameFlag> allFlags;
 
+    Dictionary<string, GameFlag> flagsByName; 
+
     public static FlagManager Instance { get; private set; }
 
     void Awake()
@@ -13,14 +15,20 @@ public class FlagManager : MonoBehaviour
         Instance = this;    
     }
 
+    void Start()
+    {
+        flagsByName = allFlags.ToDictionary(k => k.name,
+                                            v => v);    
+    }
+
     public void Set(string flagName, string value)
     {
-        var flag = allFlags.FirstOrDefault(t => t.name == flagName);
-        if(flag == null) 
+        if(flagsByName.TryGetValue(flagName, out var flag) == false) 
         {
             Debug.LogError($"flag not found {flagName}");
             return;
         }
+
         if(flag is IntGameFlag intGameFlag)
         {
             if (int.TryParse(value, out var intGameValue))
