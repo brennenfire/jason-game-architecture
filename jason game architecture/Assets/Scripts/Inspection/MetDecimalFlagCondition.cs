5006b9db5253c1cd844b9a65ea082bfd70930783
brennenfire <barbustefan2002@gmail.com>
2023-06-26 23:54:20 +0300

    added metintflag and decimal conditions

---
+++ jason game architecture/Assets/Scripts/Inspection/MetDecimalFlagCondition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MetDecimalFlagCondition : MonoBehaviour, IMet
+{
+    [SerializeField] DecimalGameFlag requiredFlag;
+    [SerializeField] decimal requiredValue;
+
+    public bool Met()
+    {
+        return requiredFlag.Value >= requiredValue;
+    }
+}
---
+++ jason game architecture/Assets/Scripts/Inspection/MetIntFlagCondition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MetIntFlagCondition : MonoBehaviour, IMet
+{
+    [SerializeField] IntGameFlag requiredFlag;
+    [SerializeField] int requiredValue;
+
+    public bool Met()
+    {
+        return requiredFlag.Value >= requiredValue;
+    }
+
+}
