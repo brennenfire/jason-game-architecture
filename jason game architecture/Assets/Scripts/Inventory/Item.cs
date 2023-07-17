5bb478da5ee7e14d3eb5a3f4c4fb4ec927f02b7d
brennenfire <barbustefan2002@gmail.com>
2023-07-17 23:44:13 +0300

    created item scriptable object and added button icons

---
+++ jason game architecture/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public Sprite Icon;
+    public string Name;
+    public string Description;
+}
