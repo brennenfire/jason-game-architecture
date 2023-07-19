aa810faf2ea35cce1fde1368e5d37259a910eb1f
brennenfire <barbustefan2002@gmail.com>
2023-07-19 23:57:55 +0300

    added itemslot

--- jason game architecture/Assets/Scripts/Inventory/Inventory.cs
+++ jason game architecture/Assets/Scripts/Inventory/Inventory.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public List<Item> Items = new List<Item>();
+    const int GENERAL_SIZE = 9;
+
+    public ItemSlot[] GeneralInventory = new ItemSlot[GENERAL_SIZE]; 
+
     [SerializeField] Item debugItem;
+
+    void Awake()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            GeneralInventory[i] = new ItemSlot();
+        }    
+    }
 
     public void AddItem(Item item)
     {
-        Items.Add(item);
+        var firstAvailableSlot = GeneralInventory.FirstOrDefault(t => t.isEmpty);
+        firstAvailableSlot.SetItem(item);
     }
 
     [ContextMenu(nameof(AddDebugItem))]
---
+++ jason game architecture/Assets/Scripts/Inventory/ItemSlot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public class ItemSlot
+{
+    public Item Item;
+
+    public bool isEmpty => Item == null;
+
+    public void SetItem(Item item)
+    {
+        Item = item;       
+    }
+}
