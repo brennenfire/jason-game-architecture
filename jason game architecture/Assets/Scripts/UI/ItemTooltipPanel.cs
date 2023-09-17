9056900af524ba71c0628e759ba94b12b719d110
brennenfire <barbustefan2002@gmail.com>
2023-09-17 23:52:49 +0300

    tooltip panel dissapear when clicking empty slots

--- jason game architecture/Assets/Scripts/UI/ItemTooltipPanel.cs
+++ jason game architecture/Assets/Scripts/UI/ItemTooltipPanel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,21 +7,39 @@ using UnityEngine.UI;
 
 public class ItemTooltipPanel : MonoBehaviour
 {
+    public static ItemTooltipPanel Instance { get; private set; }
+
     [SerializeField] TMP_Text name;
     [SerializeField] TMP_Text description;
     [SerializeField] Image icon;
 
-    public static ItemTooltipPanel Instance { get; private set; }
+    CanvasGroup canvasGroup;
 
     void Awake()
     {
-        Instance = this;    
+        Instance = this;
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void ShowItem(Item item)
     {
-        name.SetText(item.name);
-        description.SetText(item.Description);
-        icon.sprite = item.Icon;
+        if (item == null)
+        {
+            Toggle(false);
+        }
+        else
+        {
+            Toggle(true);
+            name.SetText(item.name);
+            description.SetText(item.Description);
+            icon.sprite = item.Icon;
+        }
+    }
+
+    void Toggle(bool visible)
+    {
+        canvasGroup.alpha = visible ? 1f : 0f;
+        canvasGroup.interactable = visible;
+        canvasGroup.blocksRaycasts = visible;
     }
 }
