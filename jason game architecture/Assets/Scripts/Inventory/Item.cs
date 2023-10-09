61f4f6ff31a217dce040a6975fbc2117575bc359
brennenfire <barbustefan2002@gmail.com>
2023-10-09 23:34:09 +0300

    created a control panel prefab and assigned to item

--- jason game architecture/Assets/Scripts/Inventory/Item.cs
+++ jason game architecture/Assets/Scripts/Inventory/Item.cs
@@ -13,6 +13,8 @@ public class Item : ScriptableObject
 
     public int MaxStackSize;
 
+    public GameObject PlaceablePrefab;
+
    [ContextMenu("Add 1")] public void Add1() => Inventory.Instance.AddItem(this);
    [ContextMenu("Add 5")] public void Add5() { for (int i = 0; i < 5; i++) { Inventory.Instance.AddItem(this); } }
    [ContextMenu("Add 10")] public void Add10() { for (int i = 0; i < 10; i++) { Inventory.Instance.AddItem(this); } }
