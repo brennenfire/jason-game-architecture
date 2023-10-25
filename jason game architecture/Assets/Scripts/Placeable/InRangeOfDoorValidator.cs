e9747f6f7ef0f70014352e827052f1b9d9b9a6d3
brennenfire <barbustefan2002@gmail.com>
2023-10-25 23:47:19 +0300

    broken validator

---
+++ jason game architecture/Assets/Scripts/Placeable/InRangeOfDoorValidator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class InRangeOfDoorValidator : MonoBehaviour, IValidatePlacement
+{
+    [SerializeField] float range = 10f;
+
+    public bool IsValid()
+    {
+        return (Vector3.Distance(transform.position, FindObjectOfType<GameEventListener>().transform.position) < range);
+    }
+}
