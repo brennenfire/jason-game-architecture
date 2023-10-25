5744b4d2ccf9d639e1d9e4e06bf0b1e9858a5f39
brennenfire <barbustefan2002@gmail.com>
2023-10-25 23:59:02 +0300

    working validator

--- jason game architecture/Assets/Scripts/Placeable/InRangeOfDoorValidator.cs
+++ jason game architecture/Assets/Scripts/Placeable/InRangeOfDoorValidator.cs
@@ -1,11 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class InRangeOfDoorValidator : MonoBehaviour, IValidatePlacement
 {
     [SerializeField] float range = 10f;
+    GameObject[] results;
 
     public bool IsValid()
     {
-        return (Vector3.Distance(transform.position, FindObjectOfType<GameEventListener>().transform.position) < range);
+        results = GameObject.FindGameObjectsWithTag("Door");
+
+        return (results.Any(door => Vector3.Distance(transform.position, door.transform.position) < range));
     }
 }
