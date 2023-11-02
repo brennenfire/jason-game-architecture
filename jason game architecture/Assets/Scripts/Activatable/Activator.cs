b5c529844791db4ac862830d266c83c9a5af136c
brennenfire <barbustefan2002@gmail.com>
2023-11-02 23:44:30 +0200

    added and nearest and all option to activator and split place toggle panels into child prefabs

--- jason game architecture/Assets/Scripts/Activatable/Activator.cs
+++ jason game architecture/Assets/Scripts/Activatable/Activator.cs
@@ -5,19 +5,43 @@ using UnityEngine;
 
 public class Activator : MonoBehaviour
 {
+    [SerializeField] ActivatorMode mode;
+
     [SerializeField] float radius = 10f;
     [SerializeField] string activatableTag;
 
     public void Activate()
     {
-        var allActivatablesMatchingTag = FindObjectsOfType<Activatable>()
+        var activatables = FindObjectsOfType<Activatable>()
             .Where(t => t.CompareTag(activatableTag));
 
-        var allActivatablesInRange = allActivatablesMatchingTag
-            .Where(t => Vector3.Distance(t.transform.position, transform.position) <= radius);
-        foreach (var activatable in allActivatablesInRange)
+        switch (mode)
+        {
+            case ActivatorMode.Nearest:
+                activatables = activatables
+                .OrderBy(t => Vector3.Distance(t.transform.position, transform.position))
+                .Take(1);
+                break;
+            case ActivatorMode.All:
+                break;
+            case ActivatorMode.AllInRadius:
+                activatables = activatables
+                .Where(t => Vector3.Distance(t.transform.position, transform.position) <= radius);
+                break;
+            default:
+                break;
+        }
+
+        foreach (var activatable in activatables)
         {
             activatable.Toggle();
         }
     }
+}
+
+public enum ActivatorMode
+{
+    Nearest,
+    All,
+    AllInRadius
 }
