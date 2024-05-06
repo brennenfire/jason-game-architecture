63e5baa80b06a9ffaf561a2d5f14eca4063a77ef
brennenfire <barbustefan2002@gmail.com>
2024-05-06 23:43:27 +0300

    help

--- jason game architecture/Assets/Scripts/Stats/Stat.cs
+++ jason game architecture/Assets/Scripts/Stats/Stat.cs
@@ -1,48 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Stat
+[CreateAssetMenu(menuName = "Stat Type")]
+public class StatType : ScriptableObject
 {
-    StatData statDataLocal;
-    List<StatMod> mods = new List<StatMod>();
-    public StatType StatType { get; private set; }
-
-    public string Name => StatType.name;
-
-    
-
-    public Stat(StatType statType, StatData data)
-    {
-        StatType = statType;
-        statDataLocal = data;
-    }
-
-    public float GetValue()
-    {
-        var totalValue = mods.Sum(t => t.Value) + StatType.DefaultValue + statDataLocal.Value;
-        totalValue = Math.Max(totalValue, StatType.MinimumValue);
-        if(StatType.AllowDecimals < 1)
-        {
-            return Mathf.RoundToInt(totalValue);
-        }
-
-        return totalValue;
-    }
-
-    public void AddStatMod(StatMod statMod)
-    {
-        mods.Add(statMod);
-    }
-
-    public void RemoveStatMod(StatMod statMod)
-    {
-        mods.Remove(statMod);
-    }
-
-    public void ModifyStatData(float amount)
-    {
-        statDataLocal.Value += amount;
-    }
+    public int AllowDecimals = 0;
+    public int DefaultValue = 1;
+    public int MinimumValue = 0;
 }
