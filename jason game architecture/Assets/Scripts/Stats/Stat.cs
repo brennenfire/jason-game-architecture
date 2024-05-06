8226b1859df325452d49a2eb4eb69870f4af01c8
brennenfire <barbustefan2002@gmail.com>
2024-05-06 23:45:43 +0300

    small changes

--- jason game architecture/Assets/Scripts/Stats/Stat.cs
+++ jason game architecture/Assets/Scripts/Stats/Stat.cs
@@ -1,9 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
-[CreateAssetMenu(menuName = "Stat Type")]
-public class StatType : ScriptableObject
+public class Stat
 {
-    public int AllowDecimals = 0;
-    public int DefaultValue = 1;
-    public int MinimumValue = 0;
+    StatData statDataLocal;
+    List<StatMod> mods = new List<StatMod>();
+    public StatType StatType { get; private set; }
+
+    public string Name => StatType.name;
+
+    
+
+    public Stat(StatType statType, StatData data)
+    {
+        StatType = statType;
+        statDataLocal = data;
+    }
+
+    public float GetValue()
+    {
+        var totalValue = mods.Sum(t => t.Value) + StatType.DefaultValue + statDataLocal.Value;
+        totalValue = Math.Max(totalValue, StatType.MinimumValue);
+        if(StatType.AllowDecimals < 1)
+        {
+            return Mathf.RoundToInt(totalValue);
+        }
+
+        return totalValue;
+    }
+
+    public void AddStatMod(StatMod statMod)
+    {
+        mods.Add(statMod);
+    }
+
+    public void RemoveStatMod(StatMod statMod)
+    {
+        mods.Remove(statMod);
+    }
+
+    public void ModifyStatData(float amount)
+    {
+        statDataLocal.Value += amount;
+    }
 }
---
+++ jason game architecture/Assets/Scripts/Stats/StatType.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Stat Type")]
+public class StatType : ScriptableObject
+{
+    public int AllowDecimals = 0;
+    public int DefaultValue = 1;
+    public int MinimumValue = 0;
+}
